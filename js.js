0123dbd2d40aa6e6af138f66b07ae4d61004c3c4
h205p2 <daichiuzawa@students.berkeley.net>
2017-05-18 10:06:12 -0700

    asdf

--- js.js
+++ js.js
@@ -1,3 +1,19 @@
 /**
  * Created by h205p2 on 5/18/17.
  */
+$(document).ready(function(){
+    $.ajax({
+        url: ["http://api.dronestre.am/data"],
+        type: 'GET',
+        crossDomain: true,
+        dataType: 'jsonp',
+        success: function(result) {
+            console.log(result)
+
+        },
+        error: function() {
+            alert('Failed!');
+        }
+    });
+});
+
